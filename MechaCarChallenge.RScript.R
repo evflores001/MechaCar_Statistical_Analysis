20cab8ed67e249208619b554813c0d4bddb42b68
evflores001 <67989268+evflores001@users.noreply.github.com>
2021-01-27 00:25:56 -0800

    Update MechaCarChallenge.RScript.R

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -13,6 +13,6 @@ lot_summary <- coils %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI
 
 t.test(df2$PSI, mu = 1500)
 ?t.test()
-t.test(subset(df2, Manufacturing_Lot == "Lot1", select = PSI), mu = 1500)
-t.test(subset(df2, Manufacturing_Lot == "Lot2", select = PSI), mu = 1500)
-t.test(subset(df2, Manufacturing_Lot == "Lot3", select = PSI), mu = 1500)
+t.test(subset(coils, Manufacturing_Lot == "Lot1", select = PSI), mu = 1500)
+t.test(subset(coils, Manufacturing_Lot == "Lot2", select = PSI), mu = 1500)
+t.test(subset(coils, Manufacturing_Lot == "Lot3", select = PSI), mu = 1500)
